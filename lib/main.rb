bb5b5cbd229145e02c64aa9bfc1b1687439cf813
BuildTools <gameanana@gmail.com>
2017-11-18 15:01:04 +0100

    🎁 Add ranks message

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,6 @@
 require 'discordrb'
 require_relative 'commands/info_command'
+require_relative 'commands/rank_command'
 
 unless ARGV.size == 1
   puts 'Invalid args size'
@@ -7,5 +8,9 @@ unless ARGV.size == 1
 end
 
 bot = Discordrb::Bot.new(token: ARGV[0])
+
+# Commands initialization
 InfoCommand.new(bot, 'info')
+RankCommand.new(bot, 'rank')
+
 bot.run
